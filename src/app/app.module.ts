1a79ad89002a1924be0cb29d0a4833bb72380ec3
patrick andrews <patrickxandrews@gmail.com>
2021-01-15 09:28:36 -0700

    boilerplate components services models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
