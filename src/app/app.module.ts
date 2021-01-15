5c96231079e6075f81d6a5e8cea4164d87956515
patrick andrews <patrickxandrews@gmail.com>
2021-01-15 11:07:40 -0700

    styled contacts better with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { ContactListComponent } from "./components/contact-list/contact-list.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, ContactComponent, ContactListComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
