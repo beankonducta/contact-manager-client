a404652ff32e7238b7a9f780af51569d28faf432
patrick andrews <patrickxandrews@gmail.com>
2021-01-15 10:56:27 -0700

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { ContactListComponent } from "./components/contact-list/contact-list.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ContactComponent, ContactListComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
